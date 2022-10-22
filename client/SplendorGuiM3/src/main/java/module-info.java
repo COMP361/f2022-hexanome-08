47f4a9a2d7978a1a2c3707e0e7582529d0d66147
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-10-21 20:13:20 -0400

    delete basic GUI start-off template

--- client/SplendorGuiM3/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.result.splendorguim3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.result.splendorguim3 to javafx.fxml;
-    exports com.result.splendorguim3;
-}
