455307fb086ad2fa370a9cc1b8997e713fe9c754
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-10-21 22:54:44 -0400

    added comments to GUI template start off

--- client/src/main/java/group08/App.java
+++ client/src/main/java/group08/App.java
@@ -16,7 +16,11 @@ public class App extends Application {
     // The default scene used to display the initial window
     private static Scene scene;
 
-
+    /**
+     * Override the start() method to launch the whole project
+     * @param stage
+     * @throws IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("splendor"), 640, 400);
@@ -25,10 +29,23 @@ public class App extends Application {
         stage.show();
     }
 
+    /**
+     * Replace the current scene with the scene loaded from input fxml file with the same layout (640,400 by default)
+     * @param fxml
+     * @throws IOException
+     */
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
+    /**
+     * Load a Scene from the fxml file to a new Stage with input height and width and title
+     * @param fxml
+     * @param height
+     * @param width
+     * @param title
+     * @throws IOException
+     */
     static void setRootWithSizeTitle(String fxml, int height, int width, String title) throws IOException{
         Stage newStage = new Stage();
         newStage.setTitle(title);
@@ -36,12 +53,22 @@ public class App extends Application {
         newStage.show();
     }
 
+    /**
+     * Load an fxml file and return a Parent
+     * @param fxml
+     * @return A Parent that was loaded from the fxml file
+     * @throws IOException
+     */
     // Open another new stage as same size as the initial game stage
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
+    /**
+     * Program entry point
+     * @param args
+     */
     public static void main(String[] args) {
         launch();
     }
--- client/src/main/java/group08/PrimaryController.java
+++ client/src/main/java/group08/PrimaryController.java
@@ -4,14 +4,10 @@ import java.io.IOException;
 import java.util.Objects;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -32,81 +28,86 @@ public class PrimaryController {
     @FXML
     private Label logInPageErrorMessage;
 
+    /**
+     * The logic of handling log in. The methods check if the user has input both username and user password or not
+     * @throws IOException IOException if fxml file not found
+     */
     @FXML
     protected void onLogInButtonClick() throws IOException {
-//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("splendor_game_board.fxml")));
-
-        /*
-        TODO: before set up and display the scene, should check logging condition correctness
-         */
-
         String userNameStr = userName.getText();
         String userPasswordStr = userPassword.getText();
-
 
         // For the sake of simplicity, we only check if password and username exist
         if (Objects.equals(userNameStr, "") || Objects.equals(userPasswordStr, "")){
             logInPageErrorMessage.setText("Please enter a username and password");
         }
 
-        else {
-            App.setRoot("splendor_game_board");
-        }
+        else App.setRoot("splendor_game_board");
 
     }
 
+    /**
+     * Go back (reset the scene) to the Scene loaded from splendor.fxml
+     * @throws IOException IOException if fxml file not found
+     */
     @FXML
     protected void onLogOutMenuItemClick() throws IOException{
         App.setRoot("splendor");
     }
 
+    /**
+     * Close the current stage once the quitGameButton has been clicked
+     */
     @FXML
     protected void onQuitGameButtonClick(){
-        Stage window = (Stage) quitGameButton.getScene().getWindow();
-        window.close();
+        Stage curStage = (Stage) quitGameButton.getScene().getWindow();
+        curStage.close();
     }
 
+    /**
+     * Displays the options of actions the player can do once they click on a regular development card (not orient)
+     * @throws IOException IOException if fxml file not found
+     */
     @FXML
     protected void onDevelopmentCardShapeClick() throws IOException {
-//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("splendor_purchase.fxml")));
-        App.setRootWithSizeTitle("splendor_purchase",360,170,"Make your decision");
-//        Stage stage = new Stage();
-//        stage.setTitle("Make your decision");
-//        stage.setScene(new Scene(root, 360, 170));
-//        stage.show();
+        App.setRootWithSizeTitle("splendor_card_action",360,170,"Make your decision");
+
     }
 
-
+    /**
+     * The logic to handle player purchasing a regular development card (not orient)
+     */
     @FXML
     protected void madePurchase(){
-        Stage window = (Stage) purchaseContent.getScene().getWindow();
+        Stage curStage = (Stage) purchaseContent.getScene().getWindow();
 
         /* TODO: Check condition if the purchase can be done successfully
             then close the window
          */
-        window.close();
+        curStage.close();
     }
 
+    /**
+     * The logic to handle player close the pop-up Stage when they want to cancel their action
+     */
     @FXML
     protected void cancelAction(){
-        Stage window = (Stage) purchaseContent.getScene().getWindow();
+        Stage curStage = (Stage) purchaseContent.getScene().getWindow();
         // Just close the window without doing anything
-        window.close();
+        curStage.close();
 
     }
 
+    /**
+     * The logic to handle Reserving Card (both orient and normal card can use this method)
+     */
     @FXML
     protected void madeReserve(){
-        Stage window = (Stage) purchaseContent.getScene().getWindow();
+        Stage curStage = (Stage) purchaseContent.getScene().getWindow();
 
         /* TODO: Check condition if the reserve can be done successfully
             then close the window
          */
-        window.close();
-
-    }
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        curStage.close();
     }
 }
