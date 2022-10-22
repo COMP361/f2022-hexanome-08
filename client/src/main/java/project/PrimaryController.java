98596a01ef4efe1cf28945e9c63ca336f070288f
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-10-22 00:01:40 -0400

    fixed a magic bug that stops mvn from running

--- client/src/main/java/project/PrimaryController.java
+++ client/src/main/java/project/PrimaryController.java
@@ -15,101 +15,101 @@ import javafx.stage.Stage;
  */
 public class PrimaryController {
 
-  @FXML
-  private Pane purchaseContent;
+    @FXML
+    private Pane purchaseContent;
 
-  @FXML
-  private Button quitGameButton;
+    @FXML
+    private Button quitGameButton;
 
-  @FXML
-  private TextField userName;
+    @FXML
+    private TextField userName;
 
-  @FXML
-  private PasswordField userPassword;
+    @FXML
+    private PasswordField userPassword;
 
-  @FXML
-  private Label logInPageErrorMessage;
+    @FXML
+    private Label logInPageErrorMessage;
 
-  /**
-   * The logic of handling log in. The methods check if
-   * the user has input both username and user password or not
-   *
-   * @throws IOException IOException if fxml file not found
-   */
-  @FXML
-  protected void onLogInButtonClick() throws IOException {
-    String userNameStr = userName.getText();
-    String userPasswordStr = userPassword.getText();
+    /**
+     * The logic of handling log in. The methods check if
+     * the user has input both username and user password or not
+     *
+     * @throws IOException IOException if fxml file not found
+     */
+    @FXML
+    protected void onLogInButtonClick() throws IOException {
+        String userNameStr = userName.getText();
+        String userPasswordStr = userPassword.getText();
 
-    // For the sake of simplicity, we only check if password and username exist
-    if (Objects.equals(userNameStr, "") || Objects.equals(userPasswordStr, "")) {
-      logInPageErrorMessage.setText("Please enter a username and password");
-    } else {
-      App.setRoot("splendor_game_board");
+        // For the sake of simplicity, we only check if password and username exist
+        if (Objects.equals(userNameStr, "") || Objects.equals(userPasswordStr, "")) {
+            logInPageErrorMessage.setText("Please enter a username and password");
+        } else {
+            App.setRoot("splendor_game_board");
+        }
+
     }
 
-  }
+    /**
+     * Go back (reset the scene) to the Scene loaded from splendor.fxml
+     *
+     * @throws IOException IOException if fxml file not found
+     */
+    @FXML
+    protected void onLogOutMenuItemClick() throws IOException {
+        App.setRoot("splendor");
+    }
 
-  /**
-   * Go back (reset the scene) to the Scene loaded from splendor.fxml
-   *
-   * @throws IOException IOException if fxml file not found
-   */
-  @FXML
-  protected void onLogOutMenuItemClick() throws IOException {
-    App.setRoot("splendor");
-  }
+    /**
+     * Close the current stage once the quitGameButton has been clicked.
+     */
+    @FXML
+    protected void onQuitGameButtonClick() {
+        Stage curStage = (Stage) quitGameButton.getScene().getWindow();
+        curStage.close();
+    }
 
-  /**
-   * Close the current stage once the quitGameButton has been clicked.
-   */
-  @FXML
-  protected void onQuitGameButtonClick() {
-    Stage curStage = (Stage) quitGameButton.getScene().getWindow();
-    curStage.close();
-  }
+    /**
+     * Displays the options of actions the player can do once
+     * they click on a regular development card (not orient).
+     *
+     * @throws IOException IOException if fxml file not found
+     */
+    @FXML
+    protected void onDevelopmentCardShapeClick() throws IOException {
+        App.setRootWithSizeTitle("splendor_card_action", 360, 170, "Make your decision");
 
-  /**
-   * Displays the options of actions the player can do once
-   * they click on a regular development card (not orient).
-   *
-   * @throws IOException IOException if fxml file not found
-   */
-  @FXML
-  protected void onDevelopmentCardShapeClick() throws IOException {
-    App.setRootWithSizeTitle("splendor_card_action", 360, 170, "Make your decision");
+    }
 
-  }
+    /**
+     * The logic to handle player purchasing a regular development card (not orient).
+     */
+    @FXML
+    protected void madePurchase() {
+        Stage curStage = (Stage) purchaseContent.getScene().getWindow();
+        // TODO: implement the logic of handling purchase (fail or success)
+        curStage.close();
+    }
 
-  /**
-   * The logic to handle player purchasing a regular development card (not orient).
-   */
-  @FXML
-  protected void madePurchase() {
-    Stage curStage = (Stage) purchaseContent.getScene().getWindow();
-    // TODO: implement the logic of handling purchase (fail or success)
-    curStage.close();
-  }
+    /**
+     * The logic to handle player close the pop-up Stage when they want to cancel their action.
+     */
+    @FXML
+    protected void cancelAction() {
+        Stage curStage = (Stage) purchaseContent.getScene().getWindow();
+        // Just close the window without doing anything
+        curStage.close();
 
-  /**
-   * The logic to handle player close the pop-up Stage when they want to cancel their action.
-   */
-  @FXML
-  protected void cancelAction() {
-    Stage curStage = (Stage) purchaseContent.getScene().getWindow();
-    // Just close the window without doing anything
-    curStage.close();
+    }
 
-  }
-
-  /**
-   * The logic to handle Reserving Card (both orient and normal card can use this method).
-   */
-  @FXML
-  protected void madeReserve() {
-    Stage curStage = (Stage) purchaseContent.getScene().getWindow();
-    //TODO: Check condition if the reserve can be done successfully
-    // then close the window
-    curStage.close();
-  }
+    /**
+     * The logic to handle Reserving Card (both orient and normal card can use this method).
+     */
+    @FXML
+    protected void madeReserve() {
+        Stage curStage = (Stage) purchaseContent.getScene().getWindow();
+        //TODO: Check condition if the reserve can be done successfully
+        // then close the window
+        curStage.close();
+    }
 }
