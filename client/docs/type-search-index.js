46fe2c3a96b5f62f52f2ba6adbb34db162d3ad6c
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-10-23 09:58:05 -0400

    update docs

--- client/docs/type-search-index.js
+++ client/docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"project","l":"App"},{"p":"project","l":"PrimaryController"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"project","l":"App"},{"p":"project","l":"Main"},{"p":"project","l":"PrimaryController"}];updateSearchResults();
