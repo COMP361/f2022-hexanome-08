42f30fd96a21291947973342a622ef07823072db
Bob  Jones <bobjones@Bobs-MacBook-Air.local>
2022-10-25 16:34:50 -0400

    tokens added

--- client/src/main/java/project/PrimaryController.java
+++ client/src/main/java/project/PrimaryController.java
@@ -116,23 +116,5 @@ public class PrimaryController {
 
   }
 
-  //Youngnam
-  //for buttons, hardcoded atm
-  public Button plusRed;
-  public Button minusRed;
-  public TextField counterRed;
-  public TextField TotalRed;
-  public int num =0;
-
-  //decreases red
-  public void decrementRed() {
-    num--;
-    counterRed.setText(String.valueOf(num));
-  }
-  //increased red
-  public void incrementRed() {
-    num++;
-    counterRed.setText(String.valueOf(num));
-  }
 
 }
