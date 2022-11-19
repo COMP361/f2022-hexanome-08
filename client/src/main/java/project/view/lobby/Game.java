3190fedad5fa3a3494ce771433af2f17951d30c6
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-11-19 00:32:04 -0500

    remove redundant import

--- client/src/main/java/project/view/lobby/Game.java
+++ client/src/main/java/project/view/lobby/Game.java
@@ -1,2 +1,18 @@
-package project.view.lobby;public class Game {
+package project.view.lobby;
+
+/**
+ * GameParameters object for the reply message of /api/gameservices.
+ */
+public class Game {
+  private String name;
+  private String displayName;
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
 }
