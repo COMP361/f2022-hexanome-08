be79fe578826e154864440b8ab59d1ffcf0c9a12
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-11-21 18:37:51 -0500

    created game server model draft

---
+++ server/src/main/java/ca/group8/gameservice/splendorgame/model/Board.java
@@ -0,0 +1,12 @@
+package ca.group8.gameservice.splendorgame.model;
+
+/**
+ * Generic board interface with methods common to all board game implementations. The Board represents a snapshot state
+ * of game with all information required by interacting clients. This excludes e.g. gameplay history snapshots or random
+ * generator seed values.
+ *
+ * @Author: Maximilian Schiedermeier
+ * @Date: December 2020
+ */
+public interface Board {
+}
