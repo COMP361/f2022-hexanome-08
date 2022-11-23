9392c88c656851b4eecee0f0b9f679c0ae7c9e89
juliammikk <s2092468@ed.ac.uk>
2022-11-23 17:13:46 -0500

    general fixes

--- server/src/main/java/ca/group8/gameservice/splendorgame/model/splendormodel/NobleCard.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/model/splendormodel/NobleCard.java
@@ -3,27 +3,10 @@ package ca.group8.gameservice.splendorgame.model.splendormodel;
 import java.util.EnumMap;
 
 public class NobleCard extends Card {
-    private int cardId;
-    private int prestigePoints;
-    private EnumMap<Colour,Integer> price;
-    public NobleCard(int parId, int parPrestige, EnumMap<Colour,Integer> parPrice){
-        cardId = parId;
-        prestigePoints = parPrestige;
-        price = parPrice;
+
+
+    public NobleCard(int paramPrestigePoints,
+        EnumMap<Colour, Integer> paramPrice) {
+        super(paramPrestigePoints, paramPrice);
     }
-
-    public int getCardId() {
-        return cardId;
-    }
-
-    public int getPrestigePoints() {
-        return prestigePoints;
-    }
-
-    public EnumMap<Colour, Integer> getPrice() {
-        return price;
-    }
-
-
-
 }
