7eaf81a5d61e08fbe7cc67a9b94948f1e0d20884
juliammikk <s2092468@ed.ac.uk>
2022-11-23 13:43:16 -0500

    changed cards and board again

--- server/src/main/java/ca/group8/gameservice/splendorgame/model/DevelopmentCard.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/model/DevelopmentCard.java
@@ -3,14 +3,15 @@ package ca.group8.gameservice.splendorgame.model;
 import java.util.EnumMap;
 import java.util.Optional;
 
-public class DevelopmentCard extends Card{
-    public int level;
-    public Optional<Colour> GemColor;
-    public boolean isPaired;
-    public int pairedCardId;
+public class DevelopmentCard extends Card {
 
-    public DevelopmentCard(int paramCardId, int paramPrestigePoints,
-        EnumMap<Colour, Integer> paramPrice) {
-        super(paramCardId, paramPrestigePoints, paramPrice);
-    }
+  public int level;
+  public Optional<Colour> GemColor;
+  public boolean isPaired;
+  public int pairedCardId;
+
+  public DevelopmentCard(int paramCardId, int paramPrestigePoints,
+      EnumMap<Colour, Integer> paramPrice) {
+    super(paramCardId, paramPrestigePoints, paramPrice);
+  }
 }
