82150d772a808df89c92569cdd91357a8ea640fb
youngnamh <youngnamhlady@gmail.com>
2022-11-22 19:30:18 -0500

    reformat

--- server/src/main/java/ca/group8/gameservice/splendorgame/model/NobleCard.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/model/NobleCard.java
@@ -3,10 +3,17 @@ package main.java.ca.group8.gameservice.splendorgame.model;
 import java.util.EnumMap;
 import java.util.Optional;
 
-public class NobleCard implements Card{
+/**
+ * Nobles.
+ */
+public class NobleCard implements Card {
     private int cardId;
     private int prestigePoints;
-    private EnumMap<Colour,Integer> price;
+    private EnumMap<Colour, Integer> price;
+
+    /**
+     * Constructor.
+     */
     public NobleCard(int parId, int parPrestige, EnumMap<Colour,Integer> parPrice){
         cardId = parId;
         prestigePoints = parPrestige;
