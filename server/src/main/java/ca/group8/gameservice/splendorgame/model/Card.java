dbd75183b566b79256b1209a293793fae94124f1
juliammikk <s2092468@ed.ac.uk>
2022-11-22 19:51:09 -0500

    changes to board and card

--- server/src/main/java/ca/group8/gameservice/splendorgame/model/Card.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/model/Card.java
@@ -2,8 +2,14 @@ package ca.group8.gameservice.splendorgame.model;
 
 import java.util.EnumMap;
 
-public interface Card {
-    int cardId = 0;
-    int prestigePoints = 0;
-    EnumMap<Colour,Integer> price = null;
+public class Card {
+    int cardId;
+    int prestigePoints;
+    EnumMap<Colour,Integer> price;
+
+    public Card(int paramCardId, int paramPrestigePoints, EnumMap<Colour,Integer> paramPrice){
+        cardId=paramCardId;
+        prestigePoints = paramPrestigePoints;
+        price= paramPrice;
+    }
 }
