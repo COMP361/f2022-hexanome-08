678be3363dd4cd31adec17d19f2de90b8e24aaf4
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-11-24 17:31:43 -0500

    implemented token bank gui get info logic
    Co-authored-by: muzhi<muzhi.qi@mail.mcgill.ca>
    Co-authored-by: pengyu<pengyu.xue@mail.mcgill.ca>

--- client/src/main/java/project/view/splendor/TokenBankGui.java
+++ client/src/main/java/project/view/splendor/TokenBankGui.java
@@ -1,11 +1,16 @@
 package project.view.splendor;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class TokenBankGui extends HBox {
 
@@ -18,6 +23,25 @@ public class TokenBankGui extends HBox {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public Map<Colour,Text> getColourTokenNumMap() {
+    Map<Colour,Text> resultTextList = new HashMap<>();
+    ObservableList<Node> allChildren = this.getChildren();
+    Colour[] colours =  new Colour[] {
+        Colour.RED, Colour.BLACK, Colour.WHITE, Colour.BLUE, Colour.GREEN, Colour.GOLD
+    };
+    // processing token number Text
+    for (int i = 0; i < 5; i++) {
+      Group curGroup = (Group) allChildren.get(i);
+      Text curTokenNum = (Text) curGroup.getChildren().get(4);
+      resultTextList.put(colours[i],curTokenNum);
+    }
+    VBox goldToken = (VBox) allChildren.get(5);
+    Group goldTokenGroup = (Group) goldToken.getChildren().get(1);
+    Text curTokenNum = (Text) goldTokenGroup.getChildren().get(1);
+    resultTextList.put(colours[5], curTokenNum);
+    return resultTextList;
   }
 
 }
