1f77330ada0ad768511776f5c1f41da994de5361
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-11-25 11:25:35 -0500

    added auto-generated token bank to game board

--- client/src/main/java/project/view/splendor/TokenBankGui.java
+++ client/src/main/java/project/view/splendor/TokenBankGui.java
@@ -1,13 +1,15 @@
 package project.view.splendor;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -25,18 +27,158 @@ public class TokenBankGui extends HBox {
     }
   }
 
-  public Map<Colour,Text> getColourTokenNumMap() {
-    Map<Colour,Text> resultTextList = new HashMap<>();
+  private Button getConfirmButton() {
     ObservableList<Node> allChildren = this.getChildren();
-    Colour[] colours =  new Colour[] {
-        Colour.RED, Colour.BLACK, Colour.WHITE, Colour.BLUE, Colour.GREEN, Colour.GOLD
+    VBox confirmVbox = (VBox) allChildren.get(allChildren.size()-1);
+    return (Button) confirmVbox.getChildren().get(1);
+  }
+  private EventHandler<ActionEvent> createTakeTokenHandler(
+      Label displayZone, Colour curColour) {
+    return event -> {
+      // add only if it's a valid take
+      // for this colour
+      // and for all colour
+      int curNum = Integer.parseInt(displayZone.getText());
+      int tokenLeft = Integer.parseInt(this.getColourTokenBankMap().get(curColour).getText());
+      // can only increment if 0 <= takeNum < 2 && bank > 0
+
+      int uniqueColourTakenCount = 0;
+      for (Colour c: this.getTakeTokenDecision().keySet()) {
+        int curColourTake = this.getTakeTokenDecision().get(c);
+        if (curColourTake > 0) {
+          uniqueColourTakenCount += 1;
+        }
+      }
+
+      if (curNum == 0 && uniqueColourTakenCount == 2) {
+        curNum += 1;
+        String newNum = curNum + "";
+        displayZone.setText(newNum);
+      } else {
+        if (!(curNum == 1 && uniqueColourTakenCount == 2)
+        && curNum < 2 && curNum >= 0 && tokenLeft > 0
+        && !banFromTaking(uniqueColourTakenCount)){
+          // then we can have this Label being changed
+          if(curNum == 1) {
+            if (tokenLeft >= 4) {
+              curNum += 1;
+              String newNum = curNum + "";
+              displayZone.setText(newNum);
+            }
+          } else {
+            curNum += 1;
+            String newNum = curNum + "";
+            displayZone.setText(newNum);
+          }
+
+        }
+      }
+      uniqueColourTakenCount = 0;
+      for (Colour c: this.getTakeTokenDecision().keySet()) {
+        int curColourTake = this.getTakeTokenDecision().get(c);
+        if (curColourTake > 0) {
+          uniqueColourTakenCount += 1;
+        }
+      }
+      if (banFromTaking(uniqueColourTakenCount)) {
+        getConfirmButton().setDisable(false);
+      }
     };
-    // processing token number Text
+  }
+
+  private EventHandler<ActionEvent> createBackTokenHandler(
+      Label displayZone) {
+    return event -> {
+      int curNum = Integer.parseInt(displayZone.getText());
+      // can always decrease as long as > 0
+      if (curNum > 0) {
+        // then we can have this Label being changed
+        curNum -= 1;
+        String newNum = curNum + "";
+        getConfirmButton().setDisable(true);
+        displayZone.setText(newNum);
+      }
+    };
+  }
+
+  private EventHandler<ActionEvent> createConfirmTakeTokenHandler() {
+    // TODO: Need to send the request to Game server for future use
+    return event -> {
+      Map<Colour, Text> bankTokenBankMap = getColourTokenBankMap();
+      Map<Colour, Integer> playerDecision = getTakeTokenDecision();
+      Map<Colour, Label> playerNewDecisions = getColourTokenTakeMap();
+      for (Colour c : playerDecision.keySet()) {
+        if (!c.equals(Colour.GOLD)) {
+          Text bankBalanceText = bankTokenBankMap.get(c);
+          Label playerNewChoice = playerNewDecisions.get(c);
+          int curTokenLeft = Integer.parseInt(bankBalanceText.getText());
+          int curTokenTake = playerDecision.get(c);
+          int newBankBalance = curTokenLeft - curTokenTake;
+          playerNewChoice.setText("0");
+          bankBalanceText.setText(newBankBalance+"");
+        }
+      }
+      getConfirmButton().setDisable(true);
+    };
+
+  }
+
+
+  // check whether is a valid take over all different colours
+  // use for confirmation button
+  private boolean banFromTaking(int uniqueColourTakenCount) {
+    // 3 of each different colour
+    //      1. if the colour has remaining (>0)
+    // 2 of same colour:
+    //      1. if the colour has remaining (>0)
+    //      2. if the colour has at least 4 left
+    int totalTokenTakeCount = 0;
+    for (Colour c: this.getTakeTokenDecision().keySet()) {
+      int curColourTake = this.getTakeTokenDecision().get(c);
+      if (curColourTake > 0) {
+        totalTokenTakeCount += curColourTake;
+      }
+    }
+
+    if (uniqueColourTakenCount == 3) {
+      return totalTokenTakeCount == 3;
+    } else if (uniqueColourTakenCount == 1) {
+      return totalTokenTakeCount == 2;
+    } else {
+      return false;
+    }
+  }
+
+  private Map<Colour, Label> getColourTokenTakeMap() {
+    // processing token number Text of 5 regular token
+    Map<Colour,Label> resultMap = new HashMap<>();
+    ObservableList<Node> allChildren = this.getChildren();
+    Colour[] colours =  getBaseColours();
     for (int i = 0; i < 5; i++) {
       Group curGroup = (Group) allChildren.get(i);
-      Text curTokenNum = (Text) curGroup.getChildren().get(4);
+      Label takeTokenNum = (Label) curGroup.getChildren().get(curGroup.getChildren().size()-2);
+      resultMap.put(colours[i],takeTokenNum);
+    }
+    return resultMap;
+  }
+
+  /**
+   * Return the bank balance of each token left in bank (including gold).
+   *
+   * @return a map from colour to the GUI object Text containing the bank balance
+   */
+  public Map<Colour,Text> getColourTokenBankMap() {
+    Map<Colour,Text> resultTextList = new HashMap<>();
+    ObservableList<Node> allChildren = this.getChildren();
+    Colour[] colours =  getAllColours();
+    // processing token number Text of 5 regular token
+    for (int i = 0; i < 5; i++) {
+      Group curGroup = (Group) allChildren.get(i);
+      Text curTokenNum = (Text) curGroup.getChildren().get(curGroup.getChildren().size()-1);
       resultTextList.put(colours[i],curTokenNum);
     }
+
+    // processing the gold token number Text
     VBox goldToken = (VBox) allChildren.get(5);
     Group goldTokenGroup = (Group) goldToken.getChildren().get(1);
     Text curTokenNum = (Text) goldTokenGroup.getChildren().get(1);
@@ -44,4 +186,70 @@ public class TokenBankGui extends HBox {
     return resultTextList;
   }
 
+  private Colour[] getBaseColours() {
+    return new Colour[] {
+        Colour.RED, Colour.BLACK, Colour.WHITE, Colour.BLUE, Colour.GREEN
+    };
+  }
+
+  private Colour[] getAllColours() {
+    return new Colour[] {
+        Colour.RED, Colour.BLACK, Colour.WHITE, Colour.BLUE, Colour.GREEN, Colour.GOLD
+    };
+  }
+
+
+  private void setColourTokenBankMap(Map<Colour,Integer> bankBalanceMap) {
+    Map<Colour, Text> curBankTokenBankMap = getColourTokenBankMap();
+    Colour[] allColours = getAllColours();
+    for(Colour c : allColours) {
+      Text curText = curBankTokenBankMap.get(c);
+      int newBalance = bankBalanceMap.get(c);
+      curText.setText(newBalance+"");
+    }
+  }
+
+  /**
+   * Get the take token decision of user.
+   *
+   * @return a map from colour to the GUI object Text containing the user decision
+   */
+  private Map<Colour, Integer> getTakeTokenDecision() {
+    Map<Colour, Integer> result = new HashMap<>();
+    for (Colour c : this.getColourTokenTakeMap().keySet()) {
+      int curTake = Integer.parseInt(this.getColourTokenTakeMap().get(c).getText());
+      result.put(c,curTake);
+    }
+    return result;
+  }
+
+  /**
+   * Bind all functionality to the GUI objects we care.
+   *
+   */
+  private void bindButtonAndLabel() {
+    ObservableList<Node> allChildren = this.getChildren();
+    Colour[] colours = getBaseColours();
+    // iterate through all five regular tokens
+    for (int i = 0; i < 5; i++) {
+      Group curGroup = (Group) allChildren.get(i);
+      Colour curColour = colours[i];
+      Button plusButton = (Button) curGroup.getChildren().get(1);
+      Button minusButton = (Button) curGroup.getChildren().get(2);
+      Label displayLabel = (Label) curGroup.getChildren().get(curGroup.getChildren().size()-2);
+      plusButton.setOnAction(createTakeTokenHandler(displayLabel, curColour));
+      minusButton.setOnAction(createBackTokenHandler(displayLabel));
+    }
+    Button confirmButton = getConfirmButton();
+    confirmButton.setDisable(true);
+    confirmButton.setOnAction(createConfirmTakeTokenHandler());
+  }
+
+
+  public void setup(Map<Colour, Integer> inputBankBalance, double layoutX, double layoutY) {
+    setColourTokenBankMap(inputBankBalance);
+    bindButtonAndLabel();
+    setLayoutX(layoutX);
+    setLayoutY(layoutY);
+  }
 }
