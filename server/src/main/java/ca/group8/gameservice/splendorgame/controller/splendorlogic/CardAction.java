55181964ca8dd411b1215cd46c4a77f3eafdcdf0
youngnamh <youngnamhlady@gmail.com>
2022-11-26 17:16:32 -0500

    interpreter and gameManager

--- server/src/main/java/ca/group8/gameservice/splendorgame/controller/splendorlogic/CardAction.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/controller/splendorlogic/CardAction.java
@@ -25,4 +25,35 @@ public class CardAction extends Action {
   public Card getCard() {
     return card;
   }
+
+  /*
+  1. Same class
+  2. same player
+  3. same game info
+  4. same card
+   */
+
+  // Overriding equals() to compare two Complex objects
+  @Override
+  public boolean equals(Object o) {
+
+    // If the object is compared with itself then return true
+    if (o == this) {
+      return true;
+    }
+
+        /* Check if o is an instance of Complex or not
+          "null instanceof [type]" also returns false */
+    if (!(this.getClass()==o.getClass())) {
+      return false;
+    }
+
+    // typecast o to CardAction so that we can compare data members
+    CardAction c = (CardAction) o;
+
+    // Compare the Player, Game, and Card fields
+    return this.getPlayer()==(c.getPlayer())
+            && this.getGame().equals(c.getGame())
+            && this.getCard().equals(c.getCard());
+  }
 }
