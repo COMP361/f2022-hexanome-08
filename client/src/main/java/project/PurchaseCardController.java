62ba87ab32203cc46f368954b3f42a2b74b83f6c
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-11-27 11:33:14 -0500

    added place holder for sending request to game server on purchase cards

--- client/src/main/java/project/PurchaseCardController.java
+++ client/src/main/java/project/PurchaseCardController.java
@@ -2,17 +2,60 @@ package project;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import project.view.splendor.gameitems.DevelopmentCard;
 
 public class PurchaseCardController implements Initializable {
   private final DevelopmentCard cardPurchased;
 
+  @FXML
+  private Button purchaseButton;
+
+  @FXML
+  private Button reserveButton;
+
+  @FXML
+  private Button goBackButton;
+
   public PurchaseCardController(DevelopmentCard cardPurchased) {
     this.cardPurchased = cardPurchased;
   }
+
+  private EventHandler<ActionEvent> createOnClickPurchaseHandler() {
+    // TODO: Send the request in here to the game server, for now do nothing
+    return event -> {
+      System.out.println("Sent request to server to purchase " + cardPurchased.getCardName());
+
+      Stage curWindow = (Stage) goBackButton.getScene().getWindow();
+      curWindow.close();
+    };
+  }
+
+  private EventHandler<ActionEvent> createOnClickReserveHandler() {
+    // TODO: Send the request in here to the game server, for now do nothing
+    return event -> {
+      System.out.println("Sent request to server to purchase " + cardPurchased.getCardName());
+
+      Stage curWindow = (Stage) goBackButton.getScene().getWindow();
+      curWindow.close();
+    };
+  }
+
+  private EventHandler<ActionEvent> createOnClickBackHandler() {
+    return event -> {
+      Stage curWindow = (Stage) goBackButton.getScene().getWindow();
+      curWindow.close();
+    };
+  }
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    System.out.println(cardPurchased.getCardName());
+    purchaseButton.setOnAction(createOnClickPurchaseHandler());
+    reserveButton.setOnAction(createOnClickReserveHandler());
+    goBackButton.setOnAction(createOnClickBackHandler());
   }
 }
