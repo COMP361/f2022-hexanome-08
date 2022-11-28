6a91b20acfa65202fb6af6015b63c29e4612cff3
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-11-28 16:59:17 -0500

    fixed reading JSON files into noblecard and basecard

--- server/src/main/java/ca/group8/gameservice/splendorgame/controller/Launcher.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/controller/Launcher.java
@@ -1,10 +1,4 @@
 package ca.group8.gameservice.splendorgame.controller;
-import ca.group8.gameservice.splendorgame.model.splendormodel.DevelopmentCard;
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
