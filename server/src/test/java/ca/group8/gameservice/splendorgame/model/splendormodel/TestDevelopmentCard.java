73ba457d07ef5dbb2997f5880e7940e798f684a6
youngnamh <youngnamhlady@gmail.com>
2022-11-30 16:32:21 -0500

    more unit testing

---
+++ server/src/test/java/ca/group8/gameservice/splendorgame/model/splendormodel/TestDevelopmentCard.java
@@ -0,0 +1,39 @@
+package ca.group8.gameservice.splendorgame.model.splendormodel;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class TestDevelopmentCard {
+    EnumMap<Colour, Integer> price = new EnumMap<>(Colour.class);
+    DevelopmentCard dc1;
+
+    @BeforeEach
+    void setUp(){
+        dc1 = new DevelopmentCard(2,price,"Steve",
+                2,Colour.BLUE,false,"-1",1);
+    }
+
+    @Test
+    void testGetters(){
+        assertEquals(dc1.getPrestigePoints(),2);
+        assertEquals(dc1.getPairedCardId(),"-1");
+        assertEquals(dc1.getCardName(),"Steve");
+        assertEquals(dc1.getLevel(),2);
+        assertEquals(dc1.getGemColour(),Colour.BLUE);
+        assertEquals(dc1.getGemNumber(),1);
+        assertEquals(dc1.isPaired(),false);
+    }
+
+    @Test
+    void testSetters(){
+        dc1.setPaired(true);
+        dc1.setPairedCardId("123");
+        assertEquals(dc1.isPaired(),true);
+        assertEquals(dc1.getPairedCardId(),"123");
+    }
+}
--- server/src/test/java/ca/group8/gameservice/splendorgame/model/splendormodel/TestPurchasedHand.java
+++ server/src/test/java/ca/group8/gameservice/splendorgame/model/splendormodel/TestPurchasedHand.java
@@ -1,8 +1,12 @@
 package ca.group8.gameservice.splendorgame.model.splendormodel;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.EnumMap;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TestPurchasedHand {
 
@@ -19,5 +23,28 @@ public class TestPurchasedHand {
             p1 = new PurchasedHand();
         }
 
+
+        @Test
+        void addDevelopmentTest() {
+                assertEquals(p1.getSize(),0);
+                p1.addDevelopmentCard(c1);
+                assertEquals(p1.getSize(),1);
+                assertSame(p1.getDevelopmentCards().get(0),c1);
+        }
+
+        @Test
+        void removeDevelopmentTest() {
+                p1.addDevelopmentCard(c1);
+                assertEquals(p1.getSize(),1);
+                p1.removeDevelopmentCard(c1);
+                assertEquals(p1.getSize(),0);
+        }
+
+        @Test
+        void addNobleTest() {
+                p1.addNobleCard(n1);
+                assertEquals(p1.getNobleCards().size(),1);
+                assertSame(p1.getNobleCards().get(0),n1);
+        }
 
 }
