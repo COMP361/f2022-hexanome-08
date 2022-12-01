7ab5fbe1e1f02c31a30337ccd66596b31d627b96
youngnamh <youngnamhlady@gmail.com>
2022-11-30 21:15:41 -0500

    checkstyle

--- server/src/main/java/ca/group8/gameservice/splendorgame/controller/communicationbeans/GameServerParameters.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/controller/communicationbeans/GameServerParameters.java
@@ -3,82 +3,82 @@ package ca.group8.gameservice.splendorgame.controller.communicationbeans;
 
 /**
  * Communication bean class needed for game server to talk to LS. (parsing / unparsing JSON)
- *
  */
 public class GameServerParameters {
 
-    private String name;
+  private String name;
 
-    private String displayName;
+  private String displayName;
 
-    private String location;
+  private String location;
 
-    private int maxSessionPlayers;
-    private int minSessionPlayers;
+  private int maxSessionPlayers;
+  private int minSessionPlayers;
 
-    private String webSupport;
+  private String webSupport;
 
-    public GameServerParameters(String name, String displayName, String location,
-                                int maxSessionPlayers,
-                                int minSessionPlayers, String webSupport) {
-        this.name = name;
-        this.displayName = displayName;
-        this.location = location;
-        this.maxSessionPlayers = maxSessionPlayers;
-        this.minSessionPlayers = minSessionPlayers;
-        this.webSupport = webSupport;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public int getMaxSessionPlayers() {
-        return maxSessionPlayers;
-    }
-
-    public int getMinSessionPlayers() {
-        return minSessionPlayers;
-    }
-
-    public String getWebSupport() {
-        return webSupport;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public void setMaxSessionPlayers(int maxSessionPlayers) {
-        this.maxSessionPlayers = maxSessionPlayers;
-    }
-
-    public void setMinSessionPlayers(int minSessionPlayers) {
-        this.minSessionPlayers = minSessionPlayers;
-    }
-
-    public void setWebSupport(String webSupport) {
-        this.webSupport = webSupport;
-    }
+  /**
+   * name, displayName, location, max, min, webSupport.
+   */
+  public GameServerParameters(String name, String displayName, String location,
+                              int maxSessionPlayers,
+                              int minSessionPlayers, String webSupport) {
+    this.name = name;
+    this.displayName = displayName;
+    this.location = location;
+    this.maxSessionPlayers = maxSessionPlayers;
+    this.minSessionPlayers = minSessionPlayers;
+    this.webSupport = webSupport;
+  }
 
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public int getMaxSessionPlayers() {
+    return maxSessionPlayers;
+  }
+
+  public void setMaxSessionPlayers(int maxSessionPlayers) {
+    this.maxSessionPlayers = maxSessionPlayers;
+  }
+
+  public int getMinSessionPlayers() {
+    return minSessionPlayers;
+  }
+
+  public void setMinSessionPlayers(int minSessionPlayers) {
+    this.minSessionPlayers = minSessionPlayers;
+  }
+
+  public String getWebSupport() {
+    return webSupport;
+  }
+
+  public void setWebSupport(String webSupport) {
+    this.webSupport = webSupport;
+  }
 
 
 }
