307cc34a998f144cb14b76a9516a40f6d757cb8e
youngnamh <youngnamhlady@gmail.com>
2022-11-30 20:21:25 -0500

    checkstyle

--- server/src/main/java/ca/group8/gameservice/splendorgame/model/splendormodel/BaseCard.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/model/splendormodel/BaseCard.java
@@ -3,9 +3,10 @@ package ca.group8.gameservice.splendorgame.model.splendormodel;
 import java.util.EnumMap;
 import java.util.Optional;
 
+/**
+ * This class represents development cards that are not orient.
+ */
 public class BaseCard extends DevelopmentCard {
-
-
     public BaseCard(int paramPrestigePoints,
         EnumMap<Colour, Integer> paramPrice, String cardName, int level,
         Colour gemColour, boolean isPaired, String pairedCardId, int gemNumber) {
