640b24171fd1df28e7bde65c18bd353078e14f05
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-12-05 13:23:47 -0500

    reorganized code for readability

--- client/src/main/java/project/test/People.java
+++ client/src/main/java/project/test/People.java
@@ -1,5 +1,16 @@
 package project.test;
 
+import io.github.isharipov.gson.adapters.JsonSubtype;
+import io.github.isharipov.gson.adapters.JsonType;
+
+//@JsonType(
+//    property = "type",
+//    subtypes = {
+//        @JsonSubtype(clazz = Student.class, name = "student"),
+//        @JsonSubtype(clazz = Prof.class, name = "prof")
+//    }
+//)
 public interface People {
+
   String getName();
 }
