18ede29aeea3c2bbc9d71f18aa289cfee0aa0507
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2023-03-01 21:05:30 -0500

    testing removing all model code

--- client/src/main/java/project/view/splendor/communication/DoubleGoldPower.java
+++
@@ -1,9 +0,0 @@
-package project.view.splendor.communication;
-
-public class DoubleGoldPower extends Power {
-
-    public DoubleGoldPower() {
-        super(false,PowerEffect.DOUBLE_GOLD);
-        super.type = this.getClass().getSimpleName();
-    }
-}
--- client/src/main/java/project/view/splendor/communication/FivePointsPower.java
+++
@@ -1,9 +0,0 @@
-package project.view.splendor.communication;
-
-public class FivePointsPower extends Power {
-
-    public FivePointsPower() {
-        super(false, PowerEffect.FIVE_POINTS);
-        super.type = this.getClass().getSimpleName();
-    }
-}
