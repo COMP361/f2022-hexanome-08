3bcbc759b55bd1c2e6803f8e52cee267883b8bd3
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2023-03-05 16:18:19 -0500

    added save game button and it's related popup to game board display

---
+++ client/src/main/java/project/SaveGamePopUpController.java
@@ -0,0 +1,74 @@
+package project;
+
+import ca.mcgill.comp361.splendormodel.model.GameInfo;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import project.connection.GameRequestSender;
+import project.view.lobby.communication.Savegame;
+
+public class SaveGamePopUpController implements Initializable {
+  @FXML
+  private TextField saveGameIdTextField;
+
+  @FXML
+  private Button saveButton;
+
+  @FXML
+  private Button cancelButton;
+
+  private final GameInfo gameInfo;
+  private final long gameId;
+  public SaveGamePopUpController(GameInfo gameInfo, long gameId) {
+    this.gameInfo = gameInfo;
+    this.gameId = gameId;
+  }
+
+  private EventHandler<ActionEvent> createOnClickSaveButton(Savegame savegame, String accessToken) {
+    return event -> {
+      GameRequestSender sender = App.getGameRequestSender();
+      sender.sendSaveGameRequest(gameId,savegame,accessToken);
+      Button button = (Button) event.getSource();
+      Stage curWindow = (Stage) button.getScene().getWindow();
+      curWindow.close();
+    };
+  }
+
+  private EventHandler<ActionEvent> createOnClickCancelButton() {
+    return event -> {
+      Button button = (Button) event.getSource();
+      Stage curWindow = (Stage) button.getScene().getWindow();
+      curWindow.close();
+    };
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    List<String> playerNamesList = gameInfo.getPlayerNames();
+    String[] playerNames = playerNamesList.toArray(new String[playerNamesList.size()]);
+    String gameName = App.getGameRequestSender().getGameServiceName();
+    cancelButton.setOnAction(createOnClickCancelButton());
+    Thread saveGameFiledThread = new Thread(()->{
+      while (true) {
+        // keep looping until user input something
+        String saveGameId = saveGameIdTextField.getText();
+        if (saveGameId == null || saveGameId.equals("")) {
+          saveButton.setDisable(true);
+        } else {
+          saveButton.setDisable(false);
+          Savegame savegame = new Savegame(playerNames, gameName, saveGameId);
+          String accessToken = App.getUser().getAccessToken();
+          saveButton.setOnAction(createOnClickSaveButton(savegame,accessToken));
+        }
+      }
+    });
+    saveGameFiledThread.start();
+  }
+}
