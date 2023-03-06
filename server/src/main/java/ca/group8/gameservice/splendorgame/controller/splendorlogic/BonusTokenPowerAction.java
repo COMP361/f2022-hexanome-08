f42a11ed16d1b75ae26affaae123e853d6c93eba
juliammikk <s2092468@ed.ac.uk>
2023-03-06 00:49:19 -0500

    implemented all methods and flags concerning bonus token power action

--- server/src/main/java/ca/group8/gameservice/splendorgame/controller/splendorlogic/BonusTokenPowerAction.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/controller/splendorlogic/BonusTokenPowerAction.java
@@ -1,6 +1,7 @@
 package ca.group8.gameservice.splendorgame.controller.splendorlogic;
 
 import ca.group8.gameservice.splendorgame.model.splendormodel.Card;
+import ca.group8.gameservice.splendorgame.model.splendormodel.Colour;
 import ca.group8.gameservice.splendorgame.model.splendormodel.Extension;
 import ca.group8.gameservice.splendorgame.model.splendormodel.PlayerInGame;
 import ca.group8.gameservice.splendorgame.model.splendormodel.Position;
@@ -15,55 +16,80 @@ import java.util.Map;
  */
 public class BonusTokenPowerAction extends Action {
 
-  private final PowerEffect powerEffect;
+  private Colour colour;
+  private PlayerInGame player;
 
-  public BonusTokenPowerAction(Card curCard, PowerEffect powerEffect) {
-    assert curCard != null && powerEffect != null;
-    super.type = this.getClass().getSimpleName();
-    this.powerEffect = powerEffect;
-  }
-
-  public PowerEffect getPowerEffect() {
-    return powerEffect;
+  public BonusTokenPowerAction(PlayerInGame player, Colour colour) {
+    this.player = player;
+    this.colour = colour;
   }
 
   @Override
-    // TODO
-  public void execute(TableTop curTableTop, PlayerInGame playerInGame,
-               ActionGenerator actionListGenerator,
-               ActionInterpreter actionInterpreter) {
-
-    switch (powerEffect) {
-      //TODO: no default case... added for checkstyle purposes
-      default:
-      case FIVE_POINTS:
-        playerInGame.changePrestigePoints(5);
-        break;
-      case ARM_POINTS:
-        int pointsToAdd = 0;
-        TraderBoard traderBoard = (TraderBoard) curTableTop.getBoard(Extension.TRADING_POST);
-        Map<PowerEffect, Power> playerPowers =
-            traderBoard.getAllPlayerPowers().get(playerInGame.getName());
-        //Loop through all of this Player's powers. If power is unlocked, increment count by 1.
-        for (PowerEffect power : PowerEffect.values()) {
-          if (playerPowers.get(power).isUnlocked()) {
-            pointsToAdd += 1;
-          }
-        }
-        playerInGame.changePrestigePoints(pointsToAdd);
-        //TODO: Add cases for creating cascading actions for other 3 powers.
-    }
-
+  void execute(TableTop curTableTop, PlayerInGame playerInGame, ActionGenerator actionListGenerator,
+      ActionInterpreter actionInterpreter) {
+    Map<Colour,Integer> playerTokens = player.getTokenHand().getAllTokens();
+    int oldTokenCount = playerTokens.get(colour);
+    playerTokens.put(colour, oldTokenCount+1);
+    Map<Colour,Integer> bankTokens = curTableTop.getBank().getAllTokens();
+    int oldBankTokenCount = bankTokens.get(colour);
+    bankTokens.put(colour,oldBankTokenCount-1);
   }
 
   @Override
-  Card getCurCard() throws NullPointerException {
-    throw new NullPointerException("There is no card associated with this action.");
+  Card getCurCard() {
+    return null;
   }
 
   @Override
-    //TODO: Should this have a curCard position associated with it??
   Position getCardPosition() throws NullPointerException {
-    throw new NullPointerException("There is no card position associated with this action.");
+    return null;
   }
+
+  public Colour getColour() {
+    return colour;
+  }
+
+  public PlayerInGame getPlayer() {
+    return player;
+  }
+
+//  @Override
+//    // TODO
+//  public void execute(TableTop curTableTop, PlayerInGame playerInGame,
+//               ActionGenerator actionListGenerator,
+//               ActionInterpreter actionInterpreter) {
+//
+//    switch (powerEffect) {
+//      //TODO: no default case... added for checkstyle purposes
+//      default:
+//      case FIVE_POINTS:
+//        playerInGame.changePrestigePoints(5);
+//        break;
+//      case ARM_POINTS:
+//        int pointsToAdd = 0;
+//        TraderBoard traderBoard = (TraderBoard) curTableTop.getBoard(Extension.TRADING_POST);
+//        Map<PowerEffect, Power> playerPowers =
+//            traderBoard.getAllPlayerPowers().get(playerInGame.getName());
+//        //Loop through all of this Player's powers. If power is unlocked, increment count by 1.
+//        for (PowerEffect power : PowerEffect.values()) {
+//          if (playerPowers.get(power).isUnlocked()) {
+//            pointsToAdd += 1;
+//          }
+//        }
+//        playerInGame.changePrestigePoints(pointsToAdd);
+//        //TODO: Add cases for creating cascading actions for other 3 powers.
+//    }
+//
+//  }
+//
+//  @Override
+//  Card getCurCard() throws NullPointerException {
+//    throw new NullPointerException("There is no card associated with this action.");
+//  }
+//
+//  @Override
+//    //TODO: Should this have a curCard position associated with it??
+//  Position getCardPosition() throws NullPointerException {
+//    throw new NullPointerException("There is no card position associated with this action.");
+//  }
 }
