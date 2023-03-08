922211c401d524f57eeb7de27c0a47351ab81e97
404NotFo <Muzhi.qi@mail.mcgill.ca>
2023-03-08 13:02:50 -0500

    Unfinished Trading board UI

--- client/src/main/java/project/view/splendor/TraderBoardGui.java
+++ client/src/main/java/project/view/splendor/TraderBoardGui.java
@@ -1,11 +1,22 @@
 package project.view.splendor;
 import ca.mcgill.comp361.splendormodel.actions.Action;
-import ca.mcgill.comp361.splendormodel.model.TableTop;
+import ca.mcgill.comp361.splendormodel.actions.PurchaseAction;
+import ca.mcgill.comp361.splendormodel.actions.ReserveAction;
+import ca.mcgill.comp361.splendormodel.actions.TakeTokenAction;
+import ca.mcgill.comp361.splendormodel.model.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import project.App;
+import project.GameBoardLayoutConfig;
 import project.view.InvalidDataException;
-
-import java.util.Map;
 
 public class TraderBoardGui implements BoardGui{
 
@@ -13,13 +24,20 @@ public class TraderBoardGui implements BoardGui{
   private final long gameId;
   private final AnchorPane playerBoardAnchorPane;
 
+  private final VBox powerBoardGui;
+
   public TraderBoardGui(AnchorPane playerBoardAnchorPane, long gameId) {
     this.playerBoardAnchorPane = playerBoardAnchorPane;
     this.gameId = gameId;
+    this.powerBoardGui = new PowerBoardGui(200,500);
   }
 
   @Override
   public void initialGuiActionSetup(TableTop tableTop, Map<String, Action> playerActionMap) {
+    GameBoardLayoutConfig config = App.getGuiLayouts();
+    TraderBoard traderBoard = (TraderBoard) tableTop.getBoard(Extension.TRADING_POST);
+
+
 
   }
 
