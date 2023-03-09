3049d1687f572576504a49ff1acfcd820d6bd6da
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2023-03-09 11:10:51 -0500

    junit tests and checkstyle clean up

--- server/src/main/java/ca/group8/gameservice/splendorgame/controller/splendorlogic/SplendorRestController.java
+++ server/src/main/java/ca/group8/gameservice/splendorgame/controller/splendorlogic/SplendorRestController.java
@@ -348,12 +348,6 @@ public class SplendorRestController {
     try {
       gameValidator.gameIdPlayerNameValidCheck(accessToken, playerName, gameId);
       ActionInterpreter actionInterpreter = gameManager.getGameActionInterpreter(gameId);
-      BroadcastContentManager<PlayerStates> playerStatesManager =
-          allPlayerInfoBroadcastContentManager.get(gameId);
-
-      BroadcastContentManager<GameInfo> gameInfoManger =
-          gameInfoBroadcastContentManager.get(gameId);
-
       actionInterpreter.interpretAction(actionId, playerName);
 
       // end of turn check
@@ -367,6 +361,12 @@ public class SplendorRestController {
         gameInfoBroadcastContentManager.remove(gameId);
         allPlayerInfoBroadcastContentManager.remove(gameId);
       }
+
+      BroadcastContentManager<PlayerStates> playerStatesManager =
+          allPlayerInfoBroadcastContentManager.get(gameId);
+
+      BroadcastContentManager<GameInfo> gameInfoManger =
+          gameInfoBroadcastContentManager.get(gameId);
 
       // if anything might have changed, let the client side know immediately
       playerStatesManager.touch();
